499e7ac2b1bde9594bf02c1b6d6e4e646a66dfa4
Rado Buransky <radoburansky@gmail.com>
2015-01-31 19:22:37 +0100

    Tech POST

--- app/controllers/TechController.scala
+++ app/controllers/TechController.scala
@@ -1,21 +1,36 @@
 package controllers
 
 import common.SupportedLang
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 
+case class AddTechForm(techName: String)
+
 trait TechController {
-  def add(): Action[AnyContent]
-  def all(): Action[AnyContent]
+  def add: Action[AnyContent]
+  def all: Action[AnyContent]
 }
 
 object TechController {
+  val addTechForm = Form(
+    mapping(
+      "techName" -> text
+    )(AddTechForm.apply)(AddTechForm.unapply)
+  )
+
   def apply(): TechController = new TechControllerImpl
 }
 
 private class TechControllerImpl extends Controller with TechController {
-  override def add(): Action[AnyContent] = ???
+  import TechController._
 
-  override def all(): Action[AnyContent] = Action {
-    Ok(views.html.technologies(SupportedLang.defaultLang))
+  override def add: Action[AnyContent] = Action { implicit request =>
+    val addTech = addTechForm.bindFromRequest.get
+    Ok(views.html.technologies(SupportedLang.defaultLang, addTech.techName + " added"))
+  }
+
+  override def all: Action[AnyContent] = Action {
+    Ok(views.html.technologies(SupportedLang.defaultLang, ""))
   }
 }
