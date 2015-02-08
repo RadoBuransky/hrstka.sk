b7cbd250c060e5157d68b7f206e56e253a46d179
Rado Buransky <radoburansky@gmail.com>
2015-02-08 14:18:19 +0100

    Tidy up

--- app/services/impl/TechServiceImpl.scala
+++ app/services/impl/TechServiceImpl.scala
@@ -1,7 +1,8 @@
 package services.impl
 
+import common.HEException
+import models._
 import models.db.TechVoteLog
-import models.domain
 import models.domain.Identifiable.Id
 import models.domain.TechRating
 import reactivemongo.bson.BSONObjectID
@@ -9,6 +10,7 @@ import repositories.{TechRepository, TechVoteLogRepository}
 import services.TechService
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class TechServiceImpl(techRepository: TechRepository,
                       techVoteLogRepository: TechVoteLogRepository) extends TechService {
@@ -28,19 +30,30 @@ class TechServiceImpl(techRepository: TechRepository,
         rating = TechRating(tech.upVotes,tech.downVotes))
     }.sortBy( _.rating.map(-1 * _.value).getOrElse(0.0)))
 
-  override def voteUp(id: Id, userId: Id) =  vote(id, userId, 1).map { Unit =>
-    techVoteLogRepository.logUpVote(BSONObjectID(id), BSONObjectID(userId))
-    }
+  override def voteUp(id: Id, userId: Id) = vote(id, userId, 1, techVoteLogRepository.logUpVote, canVoteUp)
+  override def voteDown(id: Id, userId: Id) = vote(id, userId, -1, techVoteLogRepository.logDownVote, canVoteDown)
 
-  override def voteDown(id: Id, userId: Id) =  vote(id, userId, -1).map { Unit =>
-    techVoteLogRepository.logDownVote(BSONObjectID(id), BSONObjectID(userId))
-  }
-
-  override def canVoteUp(id: Id, userId: Id) = canVote(id, userId, TechVoteLog.downVoteValue).map(_.getOrElse(true))
-  override def canVoteDown(id: Id, userId: Id) = canVote(id, userId, TechVoteLog.upVoteValue).map(_.getOrElse(true))
+  override def canVoteUp(id: Id, userId: Id) = canVote(id, userId, TechVoteLog.downVoteValue)
+  override def canVoteDown(id: Id, userId: Id) = canVote(id, userId, TechVoteLog.upVoteValue)
 
   private def canVote(id: Id, userId: Id, value: Int) = techVoteLogRepository.latestVote(BSONObjectID(id), BSONObjectID(userId))
-      .map(_.map(_.value == value))
+      .map(_.map(_.value == value)).map(_.getOrElse(true))
 
-  private def vote(id: Id, userId: Id, delta: Int) = techRepository.updateRating(BSONObjectID(id), delta)
+  private def vote(id: Id,
+                   userId: Id,
+                   delta: Int,
+                   logVote: (db.Identifiable.Id, db.Identifiable.Id) => Future[Unit],
+                   canVote: (Id, Id) => Future[Boolean]): Future[Unit] = {
+    canVote(id, userId).map { can =>
+      if (can) {
+        val dbId = BSONObjectID(id)
+        val dbUserId = BSONObjectID(userId)
+        techRepository.updateRating(dbId, delta).map { Unit =>
+          logVote(dbId, dbUserId)
+        }
+      }
+      else
+        throw new HEException(s"You cannot vote! [$id, $userId, $delta]")
+    }
+  }
 }
