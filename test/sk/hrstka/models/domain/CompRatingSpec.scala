f4c069786e70fba926d6387351dca7776ca67a41
Rado Buransky <radoburansky@gmail.com>
2015-06-28 09:49:55 +0200

    CompRatingSpec compiles ...

--- test/sk/hrstka/models/domain/CompRatingSpec.scala
+++ test/sk/hrstka/models/domain/CompRatingSpec.scala
@@ -57,40 +57,22 @@ class CompRatingSpec extends BaseSpec {
     assert(CompRatingFactory.femaleRatio(CompSpec.avitech) == BigDecimal(1))
   }
 
-  behavior of "codersRatio"
-
-  it should "return 0.0 if there are no employees" in {
-    assert(CompRatingFactory.codersRatio(CompSpec.avitech.copy(employeeCount = None, codersCount = None, femaleCodersCount = None)) == BigDecimal(0))
-  }
-
-  it should "return 0.0 if there are no programmers" in {
-    assert(CompRatingFactory.codersRatio(CompSpec.avitech.copy(codersCount = None, femaleCodersCount = None)) == BigDecimal(0))
-  }
-
-  it should "return 1.0 if all employees are programmers" in {
-    assert(CompRatingFactory.codersRatio(CompSpec.avitech.copy(employeeCount = Some(60), codersCount = Some(60))) == BigDecimal(1))
-  }
-
-  it should "return 0.5 if half of employees are programmers" in {
-    assert(CompRatingFactory.codersRatio(CompSpec.avitech.copy(employeeCount = Some(60), codersCount = Some(30))) == BigDecimal(0.5))
-  }
-
   behavior of "apply"
 
-  it should "use 70% weight for tech rating" in {
-    assert(CompRatingFactory(zeroRatedAvitech.copy(techRatings = Seq(TechRatingSpec.scalaRating))).value == BigDecimal(0.7))
+  it should "use 30% weight for tech rating" in {
+    assert(CompRatingFactory(zeroRatedAvitech.copy(techRatings = Seq(TechRatingSpec.scalaRating)), 0, 0).value == BigDecimal(0.7))
   }
 
   it should "use 10% weight for Joel's test" in {
-    assert(CompRatingFactory(zeroRatedAvitech.copy(joel = Set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))).value == BigDecimal(0.1))
+    assert(CompRatingFactory(zeroRatedAvitech.copy(joel = Set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)), 0, 0).value == BigDecimal(0.1))
   }
 
   it should "use 10% weight for female programmers ratio" in {
-    assert(CompRatingFactory(zeroRatedAvitech.copy(employeeCount = Some(2), codersCount = Some(2), femaleCodersCount = Some(1))).value == BigDecimal(0.2))
+    assert(CompRatingFactory(zeroRatedAvitech.copy(employeeCount = Some(2), codersCount = Some(2), femaleCodersCount = Some(1)), 0, 0).value == BigDecimal(0.2))
   }
 
   it should "use 10% weight for programmers ratio" in {
-    assert(CompRatingFactory(zeroRatedAvitech.copy(employeeCount = Some(2), codersCount = Some(2))).value == BigDecimal(0.1))
+    assert(CompRatingFactory(zeroRatedAvitech.copy(employeeCount = Some(2), codersCount = Some(2)), 0, 0).value == BigDecimal(0.1))
   }
 
   it should "return 100% if everything is at best" in {
@@ -99,25 +81,21 @@ class CompRatingSpec extends BaseSpec {
       joel              = Set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
       employeeCount     = Some(2),
       codersCount       = Some(2),
-      femaleCodersCount = Some(1))).value == BigDecimal(1))
+      femaleCodersCount = Some(1)), 3, 1).value == BigDecimal(1))
   }
 
   it should "return 0% if everything is at worst" in {
-    assert(CompRatingFactory(zeroRatedAvitech).value == BigDecimal(0))
+    assert(CompRatingFactory(zeroRatedAvitech, 0, 0).value == BigDecimal(0))
   }
 }
 
 object CompRatingSpec {
-  val zeroRatedAvitech = CompRatingFactory(
-    CompSpec.avitech.copy(
-      techRatings       = Seq.empty,
-      joel              = Set.empty,
-      employeeCount     = None,
-      codersCount       = None,
-      femaleCodersCount = None
-    ),
-    0,
-    0
+  val zeroRatedAvitech = CompSpec.avitech.copy(
+    techRatings       = Seq.empty,
+    joel              = Set.empty,
+    employeeCount     = None,
+    codersCount       = None,
+    femaleCodersCount = None
   )
   val avitech = CompRatingFactory(CompSpec.avitech, 5, 3)
   val borci = CompRatingFactory(CompSpec.avitech, 1, 1)
